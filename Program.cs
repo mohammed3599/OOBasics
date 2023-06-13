d6e8473da3239eaeab76ac4845288f290b88ec8e
mohammed3599 <kataloni1999@gmail.com>
2023-06-13 12:15:03 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace OOBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std01 = new Student("Mohammed", "A", 1111, "12SD");
+
+            //std01.name = "Mohammed";
+            //std01.grade = "B";
+            //std01.id = 123456;
+
+            Console.WriteLine("Student 01 Data...");
+            Console.WriteLine("Name: " +  std01.name);
+            Console.WriteLine("ID: " + std01.id);
+            Console.WriteLine("Grade: " + std01.grade);
+            Console.WriteLine("Registeration No: " + std01.registerationNumber);
+            std01.status();
+            Console.WriteLine();
+
+            Student std02 = new Student("Abdullah", "B", 2222, "45RT");
+
+            //std02.name = "Abdullah";
+            //std02.grade = "C";
+            //std02.id = 111222;
+
+            Console.WriteLine("Student 02 Data...");
+            Console.WriteLine("Name: " + std02.name);
+            Console.WriteLine("ID: " + std02.id);
+            Console.WriteLine("Grade: " + std02.grade);
+            Console.WriteLine("Registeration No: " + std02.registerationNumber);
+            std02.status();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOBasics
+{
+    internal class Student
+    {
+        public string name;
+        public string grade;
+        public long id;
+        public string registerationNumber;
+
+        public Student(string name, string grade, long id, string registerationNumber)
+        {
+            this.name = name;
+            this.grade = grade;
+            this.id = id;
+            this.registerationNumber = registerationNumber;
+            printStudentDetails();
+        }
+
+        public void status()
+        {
+            Console.WriteLine("Student " + name + " is studying");
+        }
+
+        void printStudentDetails ()
+        {
+            Console.WriteLine("Name: " + this.name + "Registeration No: " + this.registerationNumber);
+            Console.WriteLine();
+        }
+    }
+}
